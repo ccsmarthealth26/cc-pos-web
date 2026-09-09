b69dfaf9951b3f4e86dc5f42076cf778c6ab1794
ccsmarthealth26 <ccsmarthealth26@gmail.com>
2026-09-09 21:50:21 +0300

    Create formatCurrency.ts

---
+++ src/utils/formatCurrency.ts
@@ -0,0 +1,7 @@
+export const formatCurrency = (amount: number): string => {
+  return new Intl.NumberFormat('en-UG', {
+    style: 'currency',
+    currency: 'UGX',
+    minimumFractionDigits: 0,
+  }).format(amount);
+};
